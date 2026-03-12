daeb15813d69f1a1e3d1dcc9f00b9b11706f77f3
IT-alamm <ma9788181@gmail.com>
2026-03-12 22:52:47 +0530

    first commit

---
+++ src/main/java/com/MedSys/service/AppointmentService.java
@@ -0,0 +1,21 @@
+package com.MedSys.service;
+
+import com.MedSys.entities.Appointment;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface AppointmentService {
+
+    Appointment bookAppointment(Long patientId,
+                                Long doctorId,
+                                LocalDate date,
+                                LocalTime time);
+
+    List<Appointment> getDoctorAppointments(Long doctorId);
+
+    List<Appointment> getPatientAppointments(Long patientId);
+
+    Appointment cancelAppointment(Long appointmentId);
+}
