42a89fe55899b60e87edaa72409f7c95d7d8d892
aliirphan <aliirphan262@gmail.com>
2026-03-12 22:45:21 +0530

    first commit

---
+++ src/main/java/com/MedSys/entities/Patient.java
@@ -0,0 +1,78 @@
+package com.MedSys.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "patient")
+public class Patient {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private Long phone;
+    private Long age;
+    private String gender;
+
+    public Patient(Long age, String email, String gender, Long id, String name, Long phone) {
+        this.age = age;
+        this.email = email;
+        this.gender = gender;
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Patient() {
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+}
