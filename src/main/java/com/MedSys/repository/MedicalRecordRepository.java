1a3eab0208726dc8e65cc01fcc1477e2ed551a8d
IT-alamm <ma9788181@gmail.com>
2026-03-15 00:18:26 +0530

    second commit

---
+++ src/main/java/com/MedSys/repository/MedicalRecordRepository.java
@@ -0,0 +1,12 @@
+package com.MedSys.repository;
+
+import com.MedSys.entities.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+    List<MedicalRecord> findByPatientId(Long patientId);
+
+    List<MedicalRecord> findByDoctorId(Long doctorId);
+}
