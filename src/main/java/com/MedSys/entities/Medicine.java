20a9cf467421934b872278d678e020ce522ef64c
IT-alamm <ma9788181@gmail.com>
2026-03-15 15:21:39 +0530

    third commit

---
+++ src/main/java/com/MedSys/entities/Medicine.java
@@ -0,0 +1,74 @@
+package com.MedSys.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "medicines")
+public class Medicine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String manufacturer;
+
+    private double price;
+
+    private int stockQuantity;
+
+    // getters setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Medicine(Long id, String name, String manufacturer, double price, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+    public Medicine(){
+
+    }
+
+}
